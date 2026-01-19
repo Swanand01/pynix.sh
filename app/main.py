979b5767e3d1dd815b5df23cfa7b511604151acc
Swanand01 <swanandmathekar@gmail.com>
2026-01-19 15:53:53 +0530

    implement type command

--- app/main.py
+++ app/main.py
@@ -1,4 +1,27 @@
 import sys
+from enum import Enum
+
+
+class Command(str, Enum):
+    EXIT = 'exit'
+    ECHO = 'echo'
+    TYPE = 'type'
+
+
+command_docs = {
+    Command.EXIT: {
+        "description": "Exit the shell",
+        "usage": "exit",
+    },
+    Command.ECHO: {
+        "description": "Prints the provided arguments",
+        "usage": "echo [args...]"
+    },
+    Command.TYPE: {
+        "description": "Displays the type of the provided argument",
+        "usage": "type [arg]"
+    }
+}
 
 
 def main():
@@ -6,11 +29,17 @@ def main():
         sys.stdout.write("$ ")
         command = input()
 
-        if command == "exit":
+        if command == Command.EXIT:
             break
-        elif command.split()[0] == "echo":
+        elif command.split()[0] == Command.ECHO:
             args = command.split()[1:]
             print(" ".join(args))
+        elif command.split()[0] == Command.TYPE:
+            arg = command.split()[1]
+            if arg in list(Command):
+                print(f"{arg} is a shell builtin")
+            else:
+                print(f"{arg}: not found")
         else:
             print(f"{command}: command not found")
 
